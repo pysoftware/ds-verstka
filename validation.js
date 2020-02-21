fb7d0c2c4a87c6785b438a9f2ef91ffd18ae7b6c
Dimasta <sqzonow@mail.ru>
2020-02-21 07:13:20 -0800

    добавил js валидацию

--- validation.js
+++ validation.js
@@ -1,18 +1,51 @@
 const url = 'https://digital-spectr.com/ac/academy.php';
-const submitForm = () => {
-    $('#form').submit((event) => {
-        event.preventDefault();
-        axios.post(url, {
-            'name': 'дима',
-            'phone': '7292932',
-            'email': 'dima@mail.ru',
-        }).then(function (response) {
-            console.log(response);
-        }).catch(function (error) {
+let messages = [];
+const validation = {
+    set(target, prop, value) {
+        if (prop === 'email' && /\S+@\S+\.\S+/.test(value)) {
+            target[prop] = value;
+            return;
+        }
+        if (prop === 'name' && /[а-яА-Я]+|-|\\/.test(value)) {
+            target[prop] = value;
+            return;
+        }
+        if (prop === 'number' && /[0-9]{3}|\\|-|\\+/.test(value)) {
+            target[prop] = value;
+            return;
+        }
+        messages.push(`Invalid value of ${prop} field`);
+    }
+};
+const form = {};
+const formProxy = new Proxy(form, validation);
+
+const validateEmail = (value) => formProxy.email = value;
+const validateName = (value) => formProxy.name = value;
+const validatePhone = (value) => formProxy.number = value;
+
+$('#form').submit(function (event) {
+    event.preventDefault();
+    $(this).serializeArray().forEach(element => {
+        if (element.value.trim() === '') {
+            if (element.name !== 'email') {
+                messages.push(`${element.name} is required field`)
+            }
+        }
+    });
+    if (messages.length > 0) {
+        $('#errors').text(messages.join(', '));
+        messages = [];
+    } else {
+        $('#errors').text('');
+        console.log(form);
+        axios.post(url, form)
+            .then(function (response) {
+                console.log(response);
+            }).catch(function (error) {
             console.log(error)
         });
-        console.log('ahhah')
-    });
-};
+    }
 
-submitForm();
+});
+
